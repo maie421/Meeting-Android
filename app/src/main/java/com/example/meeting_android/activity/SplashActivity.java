defb45c42e52c8e882026d6f47fd378de855c652
윤영미 <maie421@naver.com>
2023-10-03 00:38:08 +0900

    fix : splash 글자 노출 안되는 문제 수정

--- app/src/main/java/com/example/meeting_android/activity/SplashActivity.java
+++ app/src/main/java/com/example/meeting_android/activity/SplashActivity.java
@@ -4,21 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.meeting_android.R;
 import com.example.meeting_android.common.TokenManager;
-import com.example.meeting_android.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        // 2초 후에 MainActivity로 전환
         new Handler().postDelayed(() -> {
             TokenManager tokenManager = new TokenManager(this);
             String token = tokenManager.getToken();
             if (token != null) {
-                Intent intent = new Intent(this, MainActivity.class);
+                Intent intent = new Intent(this, MeetingMainActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
             }else {
