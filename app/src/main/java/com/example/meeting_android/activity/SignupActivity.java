6413d7175d7e6b60d05993abc9b0baaf69c6fef6
윤영미 <maie421@naver.com>
2023-10-06 11:36:01 +0900

    fix : 안드로이드

--- app/src/main/java/com/example/meeting_android/activity/SignupActivity.java
+++ app/src/main/java/com/example/meeting_android/activity/SignupActivity.java
@@ -35,20 +35,20 @@ public class SignupActivity extends AppCompatActivity {
             String name = binding.nameEditText.getText().toString();
             String password = binding.passwordEditText.getText().toString();
             String checkPassword = binding.checkPasswordEditText.getText().toString();
-            if (name.isEmpty()) {
-                Toast.makeText(this, "닉네임이 빈칸입니다.", Toast.LENGTH_SHORT).show();
-                return;
-            }
             if (email.isEmpty()) {
                 Toast.makeText(this, "이메일이 빈칸입니다.", Toast.LENGTH_SHORT).show();
                 return;
             }
-            if (!isValidEmail(email)) {
-                Toast.makeText(this, "유효하지 않은 이메일 주소입니다.", Toast.LENGTH_SHORT).show();
+            if (name.isEmpty()) {
+                Toast.makeText(this, "닉네임이 빈칸입니다.", Toast.LENGTH_SHORT).show();
                 return;
             }
             if (!password.equals(checkPassword)) {
                 Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (!isValidEmail(email)) {
+                Toast.makeText(this, "유효하지 않은 이메일 주소입니다.", Toast.LENGTH_SHORT).show();
                 return;
             }
 
