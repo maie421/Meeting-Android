aac32cf96d1b4c834bd723e1fef3a1ae881c63ad
윤영미 <maie421@naver.com>
2023-10-09 12:23:04 +0900

    feat : stun 서버 적용 및 코드 정리

--- app/src/main/java/com/example/meeting_android/activity/MeetingActivity.java
+++ app/src/main/java/com/example/meeting_android/activity/MeetingActivity.java
@@ -17,6 +17,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import org.webrtc.Camera1Enumerator;
 import org.webrtc.CameraVideoCapturer;
 import org.webrtc.EglBase;
+import org.webrtc.PeerConnection;
 import org.webrtc.PeerConnectionFactory;
 import org.webrtc.RendererCommon;
 import org.webrtc.SoftwareVideoDecoderFactory;
@@ -30,29 +31,32 @@ import org.webrtc.VideoSource;
 import org.webrtc.VideoTrack;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MeetingActivity extends AppCompatActivity {
-
-
-    VideoCapturer videoCapturer;
     public static final String VIDEO_TRACK_ID = "ARDAMSv0";
-    SurfaceTextureHelper surfaceTextureHelper;
-    EglBase rootEglBase;
-    EglBase.Context eglBaseContext;
+    private static final int PERMISSION_REQUEST = 2;
+    public VideoCapturer videoCapturer;
+    public SurfaceTextureHelper surfaceTextureHelper;
+    public EglBase rootEglBase;
+    public EglBase.Context eglBaseContext;
     public PeerConnectionFactory peerConnectionFactory;
-    SurfaceViewRenderer renderer;
-    BottomNavigationView bottomNavigationView;
-
+    public SurfaceViewRenderer renderer;
+    public BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meeting);
 
-        requestPermissions();
-
         renderer = findViewById(R.id.View);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
+        onClickButtonNavigation();
+        initPeer();
+        requestPermissions();
+    }
+
+    private void onClickButtonNavigation() {
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.tab_mic) {
@@ -66,18 +70,15 @@ public class MeetingActivity extends AppCompatActivity {
             }
             return false;
         });
-
     }
 
     public void getVideoTrack(){
-
         renderer.setMirror(false);
         renderer.init(eglBaseContext,  new RendererCommon.RendererEvents() {
             //첫 번째 프레임이 렌더링되면 콜백이 실행됩니다.
             @Override
             public void onFirstFrameRendered() {
                 Log.i("RendererEvents","onFirstFrameRendered");
-//                box.setVisiProfile(false);
 
             }
             //렌더링된 프레임 해상도 또는 회전이 변경되면 콜백이 실행됩니다.
@@ -88,23 +89,29 @@ public class MeetingActivity extends AppCompatActivity {
 
         });
         rootEglBase = EglBase.create();
-        eglBaseContext= rootEglBase.getEglBaseContext();
+        eglBaseContext = rootEglBase.getEglBaseContext();
         surfaceTextureHelper = SurfaceTextureHelper.create(Thread.currentThread().getName(), eglBaseContext);
+        (getLocalVideo(true)).addSink(renderer);
+    }
 
-
+    private void initPeer() {
         PeerConnectionFactory.initialize(PeerConnectionFactory.InitializationOptions
                 .builder(getApplicationContext())
                 .setEnableInternalTracer(true)
                 .createInitializationOptions());
+
         PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
-        Log.i("options : ",options.toString());
+        Log.i("options : ", options.toString());
 
-        VideoEncoderFactory encoderFactory;
-        VideoDecoderFactory decoderFactory;
-        encoderFactory = new SoftwareVideoEncoderFactory();
-        decoderFactory = new SoftwareVideoDecoderFactory();
+        VideoEncoderFactory encoderFactory = new SoftwareVideoEncoderFactory();
+        VideoDecoderFactory decoderFactory = new SoftwareVideoDecoderFactory();
 
+        // STUN 서버 설정
+        List<PeerConnection.IceServer> iceServers = new ArrayList<>();
+        PeerConnection.IceServer stunServer = PeerConnection.IceServer.builder("stun:stun.l.google.com:19302").createIceServer();
+        iceServers.add(stunServer);
 
+        // PeerConnectionFactory 생성 및 IceServer 추가
         peerConnectionFactory = PeerConnectionFactory.builder()
                 .setOptions(options)
                 .setVideoEncoderFactory(encoderFactory)
@@ -112,17 +119,13 @@ public class MeetingActivity extends AppCompatActivity {
                 .createPeerConnectionFactory();
 
 
-        (getLocalVideo(true)).addSink(renderer);
-
     }
-
-    // 내 기기의 (카메라 소스로 얻은) video track을 반환한다.
-    public VideoTrack getLocalVideo(boolean status){
+    public VideoTrack getLocalVideo(boolean isFront){
 
         VideoTrack localVideo;
         // 앞 카메라 요청
-        videoCapturer = createCameraCapturer(status);
-        Log.w("createCameraCapturer",videoCapturer.toString());
+        videoCapturer = createVideoCapturer(isFront);
+        Log.w("createVideoCapturer",videoCapturer.toString());
         VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturer.isScreencast());
         videoCapturer.initialize(surfaceTextureHelper, getApplicationContext(), videoSource.getCapturerObserver());
 
@@ -134,12 +137,11 @@ public class MeetingActivity extends AppCompatActivity {
 
     }
 
-    private VideoCapturer createCameraCapturer(boolean isFront) {
+    private VideoCapturer createVideoCapturer(boolean isFront) {
 
         Camera1Enumerator enumerator = new Camera1Enumerator(false);
         final String[] deviceNames = enumerator.getDeviceNames();
 
-        // First, try to find front facing camera
         for (String deviceName : deviceNames) {
             if (isFront ? enumerator.isFrontFacing(deviceName) : enumerator.isBackFacing(deviceName)) {
                 VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, new CameraVideoCapturer.CameraEventsHandler() {
@@ -188,41 +190,27 @@ public class MeetingActivity extends AppCompatActivity {
 
     }
 
-
     @TargetApi(Build.VERSION_CODES.M)
     private void requestPermissions() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            // Dynamic permissions are not required before Android M.
-//onPermissionsGranted();
             return;
         }
 
         String[] missingPermissions = getMissingPermissions();
         if (missingPermissions.length != 0) {
             requestPermissions(missingPermissions, PERMISSION_REQUEST);
-        } else {
-//onPermissionsGranted();
         }
     }
-
-    private static final int PERMISSION_REQUEST = 2;
     @Override
     public void onRequestPermissionsResult(
             int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_REQUEST) {
-            String[] missingPermissions = getMissingPermissions();
-
             getVideoTrack();
-
         }
 
     }
-    private void onPermissionsGranted() {
-        // If an implicit VIEW intent is launching the app, go directly to that URL.
-        final Intent intent = getIntent();
 
-    }
     @TargetApi(Build.VERSION_CODES.M)
     private String[] getMissingPermissions() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
