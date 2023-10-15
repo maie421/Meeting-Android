6c40c72ae0e0c3f4b56c0adbd980836df4a0059c
윤영미 <maie421@naver.com>
2023-10-15 11:57:36 +0900

    form : 주석 제거

--- app/src/main/java/com/example/meeting_android/webrtc/PeerConnectionClient.java
+++ app/src/main/java/com/example/meeting_android/webrtc/PeerConnectionClient.java
@@ -81,7 +81,6 @@ public class PeerConnectionClient {
         localTrack = getLocalVideo(true);
         localTrack.addSink(localView);
         peerConnection.addTrack(localTrack);
-
     }
     void SurfaceViewRendererInit(SurfaceViewRenderer view){
         view.setMirror(false);
@@ -116,10 +115,7 @@ public class PeerConnectionClient {
             @Override
             public void onFirstFrameRendered() {
                 Log.i("RendererEvents","onFirstFrameRendered");
-//                box.setVisiProfile(false);
-
             }
-            //            렌더링된 프레임 해상도 또는 회전이 변경되면 콜백이 실행됩니다.
             @Override
             public void onFrameResolutionChanged(int i, int i1, int i2) {
                 Log.i("RendererEvents","onFrameResolutionChanged");
@@ -175,160 +171,65 @@ public class PeerConnectionClient {
         peerConnection = peerConnectionFactory.createPeerConnection(configuration, pcObserver);
     }
 
-    /**내 기기의(카메라 소스로 얻은) video track을 반환한다.
-     *
-     *@paramstatus
-     *@return:내 기기의(카메라 소스로 얻은) video track
-     */
     public VideoTrack getLocalVideo(boolean status){
-
-        // localVideo 변수 선언
         VideoTrack localVideo;
-
-        // videoCapturer : 비디오 소스에서 비디오 프레임을 캡처하고 VideoSource 객체에 전달하는 데 사용됩니다.
         videoCapturer = createCameraCapturer(status);
-        // createCameraCapturer {videoCapturer} 출력
         Log.w("createCameraCapturer",videoCapturer.toString());
 
-        // VideoSource 객체를 생성합니다. 이 VideoSource 객체는 로컬 미디어 스트림에서 비디오 소스로 사용됩니다.
-        // 1번째 매개변수는 비디오 소스가 카메라 스트림(실시간 비디오)을 사용하는지 또는 비디오 파일 스트림(미리 녹화된 비디오)을 사용하는지를 나타냅니다.
-        // videoCapturer.isScreencast() :비디오 캡처기가 화면 녹화 모드에서 동작하는 경우, 즉 사용자가 화면 공유를 수행하는 경우에 true 값을 반환합니다. 반면에 일반적인 카메라 캡처기의 경우, false 값을 반환합니다.
-        // 즉, VideoCapturer 객체가 현재 어떤 모드로 동작하는지를 나타내는
         VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturer.isScreencast());
 
-
-        //  비디오 캡처기를 초기화하고 비디오 프레임을 캡처하기 시작하는 데 사용
-        // 비디오 캡처기의 콜백 함수를 설정하고, 캡처할 비디오 해상도, 비율 및 프레임 속도 등의 속성을 설정
-        //  videoSource.getCapturerObserver() : 비디오 프레임을 전달할 CapturerObserver 객체
         videoCapturer.initialize(surfaceTextureHelper, getApplicationContext(), videoSource.getCapturerObserver());
         Log.i("videoSource.getObserver",videoSource.getCapturerObserver().toString());
 
-        // 비디오 캡쳐 : getUserMedia 로 스트림 받아오기 시작?
-        // 비디오 캡처를 시작하고, 캡처된 비디오 프레임을 VideoSink로 전달하기 위해 호출
-        // 캡처할 비디오 프레임의 너비, 높이, 및 프레임 속도
-        // 240, 320,30
         videoCapturer.startCapture(240, 320, 30);
 
-        //        이 메서드는 로컬 비디오 트랙을 생성하는 데 사용됩니다.
-        //  VideoSource 객체와 연결된 VideoTrack을 만듭니다.
-        // VIDEO_TRACK_ID은 비디오 트랙 고유 식별자로 사용.
         localVideo = peerConnectionFactory.createVideoTrack(VIDEO_TRACK_ID, videoSource);
 
-        // 내 기기의 (카메라 소스로 얻은) video track
         return localVideo;
-        // getLocalVideo 함수의 끝.
-
     }
-    /**
-     *
-     *@paramisFront
-     *@return:생성한videoCapturer반환,생성 실패시null반환.
-     */
+
     private VideoCapturer createCameraCapturer(boolean isFront) {
-
-
-        // Camera1Enumerator : Android 디바이스의 카메라 목록을 가져오고 선택한 카메라를 열기 위한 클래스
-        // 매개변수 : true를 전달하면 전면 카메라만 사용하며, false를 전달하거나 이 매개변수를 생략하면 전면 카메라와 후면 카메라 모두 사용
         Camera1Enumerator enumerator = new Camera1Enumerator(false);
 
-        // Android 디바이스에서 사용 가능한 카메라 디바이스를 열거할 수 있습니다.
-        // 카메라의 ID와 이름을 갖는 CameraEnumerationAndroid.CaptureDeviceInfo 객체의 목록을 반환합니다.
         final String[] deviceNames = enumerator.getDeviceNames();
-
-        // First, try to find front facing camera
-        // deviceNames 요소의수만큼 반복
         for (String deviceName : deviceNames) {
 
             Log.i("deviceName : ",deviceName);
-
-            // A ? B : C => 조건 연산자, A가 참이면 B를 반환, 거짓이면 C를 반환
-            // Camera1Enumerator.isFrontFacing() : 전면카메라면 true, 후면 카메라라면 false를 반환
-            // Camera1Enumerator.isBackFacing() : 후면카메라면 true, 전면 카메라라면 false를 반환
             if (isFront ? enumerator.isFrontFacing(deviceName) : enumerator.isBackFacing(deviceName)) {
-
-                // 매개변수로는 CameraEnumerationAndroid.CaptureDeviceInfo 객체와 CapturerObserver 객체, CameraEventsHandler 객체를 받습니다.
-                // deviceName 카메라로 VideoCapturer 객체 생성
-                //  선택한 카메라를 열기 위한 메소드
                 VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, new CameraVideoCapturer.CameraEventsHandler() {
-                    // CameraVideoCapturer에서 발생하는 각 이벤트를 처리하는 기본 구현을 제공하며,
-                    // 이벤트를 수신하려면 CameraVideoCapturer.setCameraEventsHandler() 메서드를 사용하여 CameraEventsHandler 객체를 등록해야 합니다.
-
-                    // 오버라이딩, 함수 재정의
                     @Override
-                    // 카메라에서 오류가 발생할 때 호출됩니다. 오류 메시지를 매개변수로 받습니다.
                     public void onCameraError(String s) {
-                        // onCameraError 메세지 출력.
-                        Log.w("onCameraError",s);
-                        // onCameraError 함수의 끝.
                     }
-
-                    // 오버라이딩, 함수 재정의
                     @Override
-                    // 카메라 연결이 끊어졌을 때 호출됩니다.
                     public void onCameraDisconnected() {
-                        // onCameraDisconnected 메세지 출력.
-                        Log.w("onCameraDisconnected","");
-                        // onCameraDisconnected 함수의 끝.
                     }
 
-                    // 오버라이딩, 함수 재정의
                     @Override
-                    // 카메라가 정지되거나 동결될 때 호출됩니다. 오류 메시지를 매개변수로 받습니다.
                     public void onCameraFreezed(String s) {
-                        // onCameraFreezed 메세지 출력.
-                        Log.w("onCameraFreezed",s);
-                        // onCameraFreezed 함수의 끝.
                     }
 
-                    // 오버라이딩, 함수 재정의
                     @Override
-                    // 카메라를 열고 있는 동안 호출됩니다. 열려고 하는 카메라의 이름을 매개변수로 받습니다.
                     public void onCameraOpening(String s) {
-                        // onCameraOpening 메세지 출력.
-                        Log.w("onCameraOpening",s);
-                        // onCameraOpening 함수의 끝.
                     }
 
-                    // 오버라이딩, 함수 재정의
                     @Override
-                    // 첫 번째 비디오 프레임이 사용 가능할 때 호출됩니다.
                     public void onFirstFrameAvailable() {
-                        // onFirstFrameAvailable 메세지 출력.
-                        Log.w("onFirstFrameAvailable","");
-                        // onFirstFrameAvailable 함수의 끝.
                     }
 
-                    // 오버라이딩, 함수 재정의
                     @Override
-                    // 카메라가 닫혔을 때 호출됩니다.
                     public void onCameraClosed() {
-                        // onCameraClosed 메세지 출력.
-                        Log.w("onCameraClosed","");
-                        // onCameraClosed 함수의 끝.
                     }
-                    // createCapturer () 끝.
                 });
 
-                // 생성성공시 {} 안의 코드 실행.
                 if (videoCapturer != null) {
-                    // 생성한 videoCapturer 반환
                     return videoCapturer;
-                    // 생성성공시 실행할 코드 끝.
                 }
-
-                // isFront가 true일 때 전면카메라, false일 때 후면 카메라면 실행할 코드 끝.
             }
-            /// 반복하는 코드의 끝.
         }
-
-        // null 반환
         return null;
-
-        /// createCameraCapturer 함수 끝.
     }
     private void pcObserver() {
         pcObserver = new PeerConnection.Observer() {
-            // SignalingState이 변경될 때 호출되는 콜백
             @Override
             public void onSignalingChange(PeerConnection.SignalingState signalingState) {
                 Log.d(TAG, "onSignalingChange : "+String.valueOf(signalingState));
@@ -418,8 +319,6 @@ public class PeerConnectionClient {
         } catch (Exception e) {
             Log.e(TAG, "Failed to get video track", e);
         }
-
-
     }
 }
 
